59be0e3aef5120aaee1575aa134a217ddee4920c
TokwatBaby <ic.janharvey.javier@cvsu.edu.ph>
2025-04-23 16:32:05 +0800

    Update Homepage.jsx

--- team-portfolio-project/src/pages/Homepage.jsx
+++ team-portfolio-project/src/pages/Homepage.jsx
@@ -4,13 +4,13 @@ import TeamMission from '../components/TeamMission';
 import TeamVision from '../components/TeamVision';
 import AboutUs from '../components/AboutUs';
 import Expertise from '../components/Expertise';
-import WelcomePage from '../components/WelcomePage.';
-import Intro from '../components/Intro'
+import WelcomePage from '../components/WelcomePage';
+import Intro from '../components/Intro';
 import Header from '../components/Header';
 import Team from '../components/Team';
-import Projects from '../components/Projects'
-import Contact from '../components/Contact'
-import Footer from '../components/Footer'
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 
 function Homepage() {
